356380324229e780e9c3cb1c50ccdf6ffb0ba88d
AstaJoks <asta.judickaite@gmail.com>
2023-08-13 01:05:04 +0000

    Create the comment model

--- blog/models.py
+++ blog/models.py
@@ -31,4 +31,18 @@ class Post(models.Model):
         return self.likes.count()
 
 
+class Comment(models.Model):
+    post = models.ForeignKey(Post, on_delete=models.CASCADE,
+                             related_name="comments")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"
 
