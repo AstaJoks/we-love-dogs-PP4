e5a9d1a9980a9f5e22f5e68ff5f2fa8595a79624
AstaJoks <asta.judickaite@gmail.com>
2023-08-18 00:18:12 +0000

    Change debug to true

--- welovedogs/settings.py
+++ welovedogs/settings.py
@@ -30,7 +30,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*', '8000-astajoks-welovedogspp4-k8q6bbzqvhw.ws-eu103.gitpod.io']
 
