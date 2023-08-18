9a2a1fa4188c6388998c5720c8a245875c81d4af
AstaJoks <asta.judickaite@gmail.com>
2023-08-18 00:11:53 +0000

    Add the like the post functionality

--- blog/urls.py
+++ blog/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('', views.Home.as_view(), name="home"),
     path('blog/', views.PostList.as_view(), name='blog'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='post_detail'),
+    path('like/<slug:slug>', views.PostLike.as_view(), name='post_like'),
 ]
